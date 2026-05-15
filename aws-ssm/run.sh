3f02faf533a0e4ea110ccf4459a5ecefb552b4f1
Raghu K <raghuk.vit@gmail.com>
2026-05-15 08:31:54 +0530

    scripts: update 1 file(s) (run.sh)

--- aws-ssm/run.sh
+++ aws-ssm/run.sh
@@ -1,1 +1,3 @@
-aws ssm get-parameters --names "/analytics-service/DATABASE_URL" --region us-east-1
+for i in $PARAMS ; do
+  aws ssm get-parameters --names "/${SERVICE_NAME}/$i" --region us-east-1 --with-decryption | jq -r '.Parameters[] | "\(.Name)=\"\(.Value)\""' | sed -e "s|/analytics-service/||" | tee -a /data/params
+done
